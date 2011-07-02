e9d733ddbe4496bb1774be21c2f2c746f332ae4f
Kenneth <Kenneth@KWF6>
2011-07-01 21:48:02 -0700

    Initial commit         Firmware empty         Most hardware errors from v1.0 fixed

---
+++ firmware/serialcouple.c
@@ -0,0 +1,13 @@
+// Kenneth Finnegan, 2011 - GPLv3
+// http://code.google.com/p/serialcouple/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+int main(void) {
+	while (1) {
+	}
+}
+
